cb6bad4236cdc8e44dee7b79d6ab9229c47ea6f9
jakobharder <jharder@live.com>
2015-05-27 21:36:10 +0200

    Added no burntime folder check (proper handler TBD)

--- source/Framework/PackageManager.cs
+++ source/Framework/PackageManager.cs
@@ -1,4 +1,31 @@
-﻿using System;
+﻿
+#region The MIT License (MIT) - 2015 Jakob Harder
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Jakob Harder
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -78,6 +105,14 @@ namespace Burntime.Framework
                 
                 // make absolute path to avoid using basePath from FileSystem
                 IPackage burntime = FileSystem.OpenPackage(System.IO.Path.GetFullPath(FileSystem.BasePath + "system/" + burntimePackage), "BURN_GFX/");
+
+                if (burntime == null)
+                {
+#warning // TODO if the package is not available then ask for a new path
+                    throw new Exception("BURN_GFX folder was not found. Please make sure to set the correct path in system/path.txt to where the BURN_GFX and BURN.EXE are!");
+                    //return;
+                }
+
                 vfs.Mount(package, burntime);
                 return;
             }
