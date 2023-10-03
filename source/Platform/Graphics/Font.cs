7905429e15e9c12e6daf6c389b917827920d363a
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2023-10-03 18:53:58 +0200

    fix location name position

--- source/Platform/Graphics/Font.cs
+++ source/Platform/Graphics/Font.cs
@@ -1,212 +1,192 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Burntime.Platform.Graphics
+namespace Burntime.Platform.Graphics;
+
+public struct FontInfo
 {
-    public struct FontInfo
-    {
-        public String Font;
-        public PixelColor ForeColor;
-        public PixelColor BackColor;
-        public bool UseBackColor;
-    }
+    public String Font;
+    public PixelColor ForeColor;
+    public PixelColor BackColor;
+    public bool UseBackColor;
+}
 
-    public enum TextAlignment
-    {
-        Left,
-        Center,
-        Right
-    }
+public enum TextAlignment
+{
+    Left,
+    Center,
+    Right
+}
 
-    public enum VerticalTextAlignment
-    {
-        Top,
-        Center,
-        Bottom
-    }
+public enum VerticalTextAlignment
+{
+    Top,
+    Center,
+    Bottom
+}
 
-    public enum TextBorders
-    {
-        None,
-        Screen,
-        Window
-    }
+public enum TextBorders
+{
+    None,
+    Screen,
+    Window
+}
 
-    public struct CharInfo
-    {
-        public int pos;
-        public int width;
-        public int imgWidth;
-        public int imgHeight;
+public struct CharInfo
+{
+    public int pos;
+    public int width;
+    public int imgWidth;
+    public int imgHeight;
 
-        public Vector2 spritePos;
-    };
+    public Vector2 spritePos;
+};
 
-    public class Font
-    {
-        public FontInfo Info;
+public class Font
+{
+    public FontInfo Info;
 
 #warning slimdx todo below for parameters were internal
-        public ISprite sprite;
-        public Dictionary<char, CharInfo> charInfo;
-        public int offset;
-        public int height;
+    public ISprite sprite;
+    public Dictionary<char, CharInfo> charInfo;
+    public int offset;
+    public int height;
 
-        TextBorders borders = TextBorders.Window;
-        public TextBorders Borders
+    public TextBorders Borders { get; set; } = TextBorders.Window;
+
+    public void DrawText(RenderTarget target, Vector2 position, string text, TextAlignment align = TextAlignment.Left, 
+        VerticalTextAlignment verticalAlign = VerticalTextAlignment.Center, float alpha = 1)
+    {
+        target.Layer++;
+        if (Info.UseBackColor)
         {
-            get { return borders; }
-            set { borders = value; }
+            DrawText(target, position, text, align, verticalAlign, new PixelColor((int)(255 * alpha), 255, 255, 255));
         }
-
-        public void DrawText(RenderTarget Target, Vector2 Position, String Text)
+        else
         {
-            DrawText(Target, Position, Text, TextAlignment.Left);
+            var c = new PixelColor((int)(Info.ForeColor.a * alpha), Info.ForeColor.r, Info.ForeColor.g, Info.ForeColor.b);
+            DrawText(target, position, text, align, verticalAlign, c);
         }
+        target.Layer--;
+    }
 
-        public void DrawText(RenderTarget Target, Vector2 Position, String Text, TextAlignment Align)
-        {
-            DrawText(Target, Position, Text, Align, VerticalTextAlignment.Center);
-        }
+    void DrawText(RenderTarget target, Vector2 position, string text, TextAlignment align, VerticalTextAlignment verticalAlign, PixelColor color)
+    {
+        // TODO: text align
+        if (text == null || text.Length == 0)
+            return;
 
-        public void DrawText(RenderTarget Target, Vector2 Position, String Text, TextAlignment Align, VerticalTextAlignment VertAlign)
-        {
-            DrawText(Target, Position, Text, Align, VertAlign, 1);
-        }
+        Vector2 offset = new Vector2(position);
 
-        public void DrawText(RenderTarget Target, Vector2 Position, String Text, TextAlignment Align, VerticalTextAlignment VertAlign, float alpha)
+        string[] lines = text.Split('\n');
+        foreach (string str in lines)
         {
-            Target.Layer++;
-            if (Info.UseBackColor)
+            if (str.Length == 0)
+                continue;
+
+            offset.x = position.x;
+
+            if (align == TextAlignment.Center)
+                offset.x -= GetRect(0, 0, str).Width / 2;
+            else if (align == TextAlignment.Right)
+                offset.x -= GetRect(0, 0, str).Width;
+
+            if (verticalAlign == VerticalTextAlignment.Center)
+                offset.y -= GetHeight() / 2;
+            else if (verticalAlign == VerticalTextAlignment.Bottom)
+                offset.y -= GetHeight();
+
+            if (Borders == TextBorders.Window)
             {
-                DrawText(Target, Position, Text, Align, VertAlign, new PixelColor((int)(255 * alpha), 255, 255, 255));
+                Vector2 lt = new Vector2();
+                Vector2 rb = lt + target.Size - new Vector2(GetWidth(str), GetHeight());
+                offset.Min(lt);
+                offset.Max(rb);
             }
-            else
+            else if (Borders == TextBorders.Screen)
             {
-                PixelColor c = new PixelColor((int)(Info.ForeColor.a * alpha), Info.ForeColor.r, Info.ForeColor.g, Info.ForeColor.b);
-                DrawText(Target, Position, Text, Align, VertAlign, c);
+                Vector2 lt = -target.ScreenOffset + 2;
+                Vector2 rb = lt + target.ScreenSize - new Vector2(GetWidth(str), GetHeight()) - 2;
+                offset.Min(lt);
+                offset.Max(rb);
             }
-            Target.Layer--;
-        }
 
-        void DrawText(RenderTarget target, Vector2 pos, String text, TextAlignment Align, VerticalTextAlignment VertAlign, PixelColor color)
-        {
-            // TODO: text align
-            if (text == null || text.Length == 0)
-                return;
-
-            Vector2 offset = new Vector2(pos);
-
-            string[] lines = text.Split('\n');
-            foreach (string str in lines)
-            {
-                if (str.Length == 0)
-                    continue;
-
-                offset.x = pos.x;
-
-                if (Align == TextAlignment.Center)
-                    offset.x -= GetRect(0, 0, str).Width / 2;
-                else if (Align == TextAlignment.Right)
-                    offset.x -= GetRect(0, 0, str).Width;
-
-                if (VertAlign == VerticalTextAlignment.Center)
-                    offset.y -= GetHeight() / 2;
-                else if (VertAlign == VerticalTextAlignment.Bottom)
-                    offset.y -= GetHeight();
-
-                if (borders == TextBorders.Window)
-                {
-                    Vector2 lt = new Vector2();
-                    Vector2 rb = lt + target.Size - new Vector2(GetWidth(str), GetHeight());
-                    offset.Min(lt);
-                    offset.Max(rb);
-                }
-                else if (borders == TextBorders.Screen)
-                {
-                    Vector2 lt = -target.ScreenOffset + 2;
-                    Vector2 rb = lt + target.ScreenSize - new Vector2(GetWidth(str), GetHeight()) - 2;
-                    offset.Min(lt);
-                    offset.Max(rb);
-                }
-
-                target.SelectSprite(sprite);
-
-                char[] charray = str.ToCharArray();
-                foreach (char ch in charray)
-                {
-                    offset.x += DrawChar(target, ch, offset, color);
-                }
-
-                offset.y += (int)(GetHeight() - this.offset);
-            }
-        }
-
-        int DrawChar(RenderTarget target, char ch, Vector2 pos, PixelColor color)
-        {
-            CharInfo info = charInfo[translateChar(ch)];
-            target.DrawSelectedSprite(pos + new Vector2(0, offset), new Rect(info.spritePos, new Vector2(info.imgWidth, info.imgHeight)), color);
-            return info.width;
-        }
-
-        public Rect GetRect(int x, int y, String str)
-        {
-            Rect rc = new Rect(x, y, 0, 0);
-            char last = '\n';
-            int width = 0;
+            target.SelectSprite(sprite);
 
             char[] charray = str.ToCharArray();
             foreach (char ch in charray)
             {
-                if (last == '\n')
-                {
-                    rc.Height += (int)(GetHeight() - offset);
-                    rc.Width = System.Math.Max(rc.Width, width);
-                    width = 0;
-                }
-                
-                if (ch != '\n')
-                {
-                    CharInfo info = charInfo[translateChar(ch)];
-                    width += info.width;
-                }
-
-                last = ch;
+                offset.x += DrawChar(target, ch, offset, color);
             }
 
-            rc.Width = System.Math.Max(rc.Width, width);
-
-            return rc;
+            offset.y += (int)(GetHeight() - this.offset);
         }
+    }
 
-        public int GetWidth(String Text)
+    int DrawChar(RenderTarget target, char ch, Vector2 pos, PixelColor color)
+    {
+        CharInfo info = charInfo[translateChar(ch)];
+        target.DrawSelectedSprite(pos + new Vector2(0, offset), new Rect(info.spritePos, new Vector2(info.imgWidth, info.imgHeight)), color);
+        return info.width;
+    }
+
+    public Rect GetRect(int x, int y, String str)
+    {
+        Rect rc = new Rect(x, y, 0, 0);
+        char last = '\n';
+        int width = 0;
+
+        char[] charray = str.ToCharArray();
+        foreach (char ch in charray)
         {
-            int width = 0;
-            char[] charray = Text.ToCharArray();
-            foreach (char ch in charray)
+            if (last == '\n')
+            {
+                rc.Height += (int)(GetHeight() - offset);
+                rc.Width = System.Math.Max(rc.Width, width);
+                width = 0;
+            }
+
+            if (ch != '\n')
             {
                 CharInfo info = charInfo[translateChar(ch)];
                 width += info.width;
             }
 
-            return width;
+            last = ch;
         }
 
-        public virtual int GetHeight()
+        rc.Width = System.Math.Max(rc.Width, width);
+
+        return rc;
+    }
+
+    public int GetWidth(String Text)
+    {
+        int width = 0;
+        char[] charray = Text.ToCharArray();
+        foreach (char ch in charray)
         {
-            return (int)((height * sprite.Resolution + offset * 2));
+            CharInfo info = charInfo[translateChar(ch)];
+            width += info.width;
         }
 
-        char translateChar(char ch)
-        {
-            if (charInfo.ContainsKey(ch))
-                return ch;
-            return '?';
-        }
+        return width;
+    }
 
-        public virtual bool IsSupportetCharacter(char ch)
-        {
-            return charInfo.ContainsKey(ch);
-        }
+    public virtual int GetHeight()
+    {
+        return (int)((height * sprite.Resolution + offset * 2));
+    }
+
+    char translateChar(char ch)
+    {
+        if (charInfo.ContainsKey(ch))
+            return ch;
+        return '?';
+    }
+
+    public virtual bool IsSupportetCharacter(char ch)
+    {
+        return charInfo.ContainsKey(ch);
     }
 }
