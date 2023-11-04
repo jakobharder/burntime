1b1e360ca8390a7419eff61f256a4d4ac287baf8
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2023-11-04 13:11:10 +0100

    allow music playback across scenes

--- source/Burntime.Framework/Scene.cs
+++ source/Burntime.Framework/Scene.cs
@@ -1,59 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Burntime.Framework.GUI;
 
-using Burntime.Platform;
-using Burntime.Platform.Graphics;
-using Burntime.Framework.GUI;
+namespace Burntime.Framework;
 
-namespace Burntime.Framework
+public abstract class Scene : Container
 {
-    public abstract class Scene : Container
+    public string? Music { get; set; }
+    public bool KeepMusic { get; set; } = false;
+
+    public Scene(Module app)
+        : base(app)
     {
-        string music;
-        public string Music
-        {
-            get { return music; }
-            set { music = value; }
-        }
-
-        public Scene(Module app)
-            : base(app)
-        {
-            Layer = 0;
-            HasFocus = true;
-        }
-
-        internal void ActivateScene()
-        {
-            ActivateScene(null);
-        }
-
-        internal void ActivateScene(object parameter)
-        {
-            OnResizeScreen();
-            OnActivateScene(parameter);
-
-            foreach (var window in Windows)
-                window.OnActivate();
-
-            if (music != null)
-                app.Engine.Music.Play(music);
-        }
-
-        internal void InactivateScene()
-        {
-            app.Engine.Music.Stop();
-
-            OnInactivateScene();
-        }
-
-        protected virtual void OnActivateScene(object parameter)
-        {
-        }
-
-        protected virtual void OnInactivateScene()
-        {
-        }
+        Layer = 0;
+        HasFocus = true;
     }
+
+    internal void ActivateScene(object? parameter = null)
+    {
+        OnResizeScreen();
+        OnActivateScene(parameter);
+
+        foreach (var window in Windows)
+            window.OnActivate();
+
+        if (!KeepMusic)
+            app.Engine.Music.Stop();
+        if (!string.IsNullOrEmpty(Music))
+            app.Engine.Music.Play(Music);
+    }
+
+    internal void InactivateScene() => OnInactivateScene();
+
+    protected virtual void OnActivateScene(object? parameter) { }
+    protected virtual void OnInactivateScene() { }
 }
