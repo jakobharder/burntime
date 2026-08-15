1f7c3490eae5b7f4ccec78abe9cb0eaeb9827339
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2026-08-15 11:37:53 +0200

    fix version scripts

--- source/Burntime.MonoGame/get-file-version.sh
+++ source/Burntime.MonoGame/get-file-version.sh
@@ -4,25 +4,21 @@ set -eu
 # v1.2.3 -> 1.2.3
 # v1.2.3-rc1 -> 1.2.3
 # v1.2.3-1-asdf -> 1.2.3.1
+# v1.2.3-rc1-1-asdf -> 1.2.3.1
 # v1.2-1-asdf -> 1.2.0.1
 
 description=$(git describe --tags)
 version=${description#v}
 version=${version%%+*}
 
-old_ifs=$IFS
-IFS=-
-set -- $version
-IFS=$old_ifs
+commit=$(printf '%s\n' "$version" | sed -nE 's/^.*-([0-9]+)-g[0-9a-f]+(-dirty)?$/\1/p')
+tag=$(printf '%s\n' "$version" | sed -E 's/-[0-9]+-g[0-9a-f]+(-dirty)?$//')
+base=${tag%%-*}
 
-base=$1
-suffix=${2-}
-commit=${3-}
-
-if [ -z "$suffix" ] || [ -z "$commit" ]; then
+if [ -z "$commit" ]; then
     printf '%s\n' "$base"
 elif [ "${base#*.*.}" = "$base" ]; then
-    printf '%s.0.%s\n' "$base" "$suffix"
+    printf '%s.0.%s\n' "$base" "$commit"
 else
-    printf '%s.%s\n' "$base" "$suffix"
+    printf '%s.%s\n' "$base" "$commit"
 fi
